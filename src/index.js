cb28010651d3e25bd6e2614dc73cf00e6b73e527
Andy Yulianto <rinovannet@gmail.com>
2025-04-10 10:57:06 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* TODO: SLIDE 31 */
+import './u-alerts.js';
+import './u-card.js';
+import './u-mark.js';
+/* TODO: SLIDE 54 */
+import './u-navbar.js';
+import './u-search.js';
+import './u-social-icon.js';
+import './u-toast.js';
+import './u-footer.js';
